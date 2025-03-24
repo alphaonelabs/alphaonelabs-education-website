23b54018dd0c8877704b2f456caf0af192cf0d6f
A1L13N <193832434+A1L13N@users.noreply.github.com>
2025-03-24 02:57:34 -0400

    Update web/settings.py

--- web/settings.py
+++ web/settings.py
@@ -38,7 +38,8 @@ TWITTER_USERNAME = "alphaonelabs"
 
 # Production settings
 if not DEBUG:
-    SECURE_SSL_REDIRECT = False
+   # turn on debug = True if this is an issue
+    SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
