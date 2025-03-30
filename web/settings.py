7cf2ea4f8f59833fdb18d61e20753e154cbc7e12
10done <anubhavtandon6424@gmail.com>
2025-03-31 03:54:41 +0530

    Security Updates

--- web/settings.py
+++ web/settings.py
@@ -7,13 +7,14 @@ from cryptography.fernet import Fernet
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = "django-insecure-5kyff0s@l_##j3jawec5@b%!^^e(j7v)ouj4b7q6kru#o#a)o3"
 
 env = environ.Env()
 env_file = os.path.join(BASE_DIR, ".env")
 if os.path.exists(env_file):
     env.read_env(env_file)
 
-SECRET_KEY = env.str("SECRET_KEY", default="django-insecure-5kyff0s@l_##j3jawec5@b%!^^e(j7v)ouj4b7q6kru#o#a)o3")
+SECRET_KEY = "django-insecure-5kyff0s@l_##j3jawec5@b%!^^e(j7v)ouj4b7q6kru#o#a)o3"
 # Set encryption key for secure messaging; in production, this must come from the environment
 MESSAGE_ENCRYPTION_KEY = env.str("MESSAGE_ENCRYPTION_KEY", default=Fernet.generate_key()).strip()
 SECURE_MESSAGE_KEY = MESSAGE_ENCRYPTION_KEY
