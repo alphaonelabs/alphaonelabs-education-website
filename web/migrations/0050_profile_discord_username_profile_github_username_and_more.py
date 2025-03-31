78fec3fd5f026293da099776fbbfde875fafb097
varruunnn <varun.choudhary2512@gmail.com>
2025-03-31 13:35:17 +0530

    feat(Add fields on profile for discord, slack and GitHub username)

--- web/migrations/0050_profile_discord_username_profile_github_username_and_more.py
+++ web/migrations/0050_profile_discord_username_profile_github_username_and_more.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('web', '0049_session_latitude_session_longitude_and_more'),
+        ("web", "0049_session_latitude_session_longitude_and_more"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='profile',
-            name='discord_username',
-            field=models.CharField(blank=True, help_text='Your Discord username (e.g., User#1234)', max_length=50),
+            model_name="profile",
+            name="discord_username",
+            field=models.CharField(blank=True, help_text="Your Discord username (e.g., User#1234)", max_length=50),
         ),
         migrations.AddField(
-            model_name='profile',
-            name='github_username',
-            field=models.CharField(blank=True, help_text='Your GitHub username (without @)', max_length=50),
+            model_name="profile",
+            name="github_username",
+            field=models.CharField(blank=True, help_text="Your GitHub username (without @)", max_length=50),
         ),
         migrations.AddField(
-            model_name='profile',
-            name='slack_username',
-            field=models.CharField(blank=True, help_text='Your Slack username', max_length=50),
+            model_name="profile",
+            name="slack_username",
+            field=models.CharField(blank=True, help_text="Your Slack username", max_length=50),
         ),
     ]
