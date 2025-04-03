2d237ddcabfc482921d1b75aeb38cfdeee531213
Inshamhaque <haqueinsham@gmail.com>
2025-04-03 19:30:40 +0530

    Fixed media files not working locally

--- web/settings.py
+++ web/settings.py
@@ -305,7 +305,6 @@ LOCALE_PATHS = [
 USE_L10N = True
 
 if os.environ.get("DATABASE_URL"):
-    DEBUG = False
     DATABASES = {"default": env.db()}
 
     # Only add MySQL-specific options if using MySQL
