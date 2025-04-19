872df436dd2c7ebe89968ba1102abe2435c458bc
Sayan <theonlysayan@gmail.com>
2025-04-19 11:41:41 +0530

    fixing

--- web/migrations/0059_selfcheckin_sessionfeedback_sessionsurvey.py
+++ web/migrations/0059_selfcheckin_sessionfeedback_sessionsurvey.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("web", "0057_alter_educationalvideo_uploader"),
+        ("web", "0058_forumtopic_github_issue_url_and_more"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
